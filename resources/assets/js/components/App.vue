1d4e7642f1cfb49c97160218d6c25406300e0ce5
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2018-02-21 22:02:05 +0200

    wip

--- resources/assets/js/components/App.vue
+++ resources/assets/js/components/App.vue
@@ -27,7 +27,24 @@
             </div>
             <div class="w-5/6">
                 <div class="shadow p-6 bg-white w-full">
-                    <h4 class="mb-2 pb-2 uppercase text-grey-darker tracking-wide"> Overview </h4>
+                    <div class="flex justify-between items-center mb-4">
+                        <h4 class="mb-2 pb-2 uppercase text-grey-darker tracking-wide"> Overview </h4>
+
+                        <div class="relative mr-6 my-2">
+                            <input type="search" class="bg-purple-white shadow rounded border-0 p-3"
+                                   placeholder="Search...">
+                            <div class="absolute pin-r pin-t mt-3 mr-4 text-purple-lighter">
+                                <svg version="1.1" class="h-4 text-dark" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                                 viewBox="0 0 52.966 52.966" style="enable-background:new 0 0 52.966 52.966;" xml:space="preserve">
+                                    <path d="M51.704,51.273L36.845,35.82c3.79-3.801,6.138-9.041,6.138-14.82c0-11.58-9.42-21-21-21s-21,9.42-21,21s9.42,21,21,21
+                                    c5.083,0,9.748-1.817,13.384-4.832l14.895,15.491c0.196,0.205,0.458,0.307,0.721,0.307c0.25,0,0.499-0.093,0.693-0.279
+                                    C52.074,52.304,52.086,51.671,51.704,51.273z M21.983,40c-10.477,0-19-8.523-19-19s8.523-19,19-19s19,8.523,19,19
+                                    S32.459,40,21.983,40z"/>
+
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
                     <div class="p-4 flex items-center justify-center" v-if="! logs">
                         <font-awesome-icon class="text-grey mr-3 text-2xl" icon="spinner" spin/>
                         Loading...
@@ -51,7 +68,9 @@
 
                                     {{log.level}}
                                 </td>
-                                <td class="p-2 border-t border-grey-light text-sm text-grey-darker" style="min-width: 100px"> {{ log.date}}</td>
+                                <td class="p-2 border-t border-grey-light text-sm text-grey-darker"
+                                    style="min-width: 100px"> {{ log.date}}
+                                </td>
                                 <td class="p-2 border-t border-grey-light text-sm text-grey-darker"> {{ log.text }}</td>
                             </tr>
                             </tbody>
--- resources/assets/js/components/TopNavigation.vue
+++ resources/assets/js/components/TopNavigation.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="py-4 text-3xl border-b-2 border-grey-light mb-8">
+    <div class="py-4 text-3xl border-b-2 border-grey-light mb-4">
         <span class="font-bold">Laravel</span> <span class="font-light">Ward</span>
     </div>
 </template>
