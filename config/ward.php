a1edd2bce1941cbca4fbddd585136785f4124bf2
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2018-09-02 11:39:04 +0300

    wip

--- config/ward.php
+++ config/ward.php
@@ -1,7 +1,10 @@
 <?php
 
 return [
-    'prefix'     => env('LOG_VIEWER_PREFIX', 'ward:'),
-    'uri'        => env('LOG_VIEWER_URI', 'ward'),
-    'middleware' => env('LOG_VIEWER_MIDDLEWARE', 'web'),
+	'enable_routes'  => env('LOG_VIEWER_ENABLE_ROUTES', true),
+	'prefix'         => env('LOG_VIEWER_PREFIX', 'ward:'),
+	'uri'            => env('LOG_VIEWER_URI', 'ward'),
+	'middleware'     => env('LOG_VIEWER_MIDDLEWARE', 'web'),
+	'logs_per_page'  => env('LOG_VIEWER_LOGS_PER_PAGE', 6),
+	'cache_duration' => env('LOG_VIEWER_CACHE_DURATION', 0),
 ];
--- src/WardServiceProvider.php
+++ src/WardServiceProvider.php
@@ -7,94 +7,96 @@ use Illuminate\Support\ServiceProvider;
 
 class WardServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $this->registerRoutes();
-        $this->registerResources();
-        $this->defineAssetPublishing();
-    }
+	/**
+	 * Bootstrap any application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->registerRoutes();
+		$this->registerResources();
+		$this->defineAssetPublishing();
+	}
 
-    /**
-     * Register the LogViewer routes.
-     *
-     * @return void
-     */
-    protected function registerRoutes()
-    {
-        Route::group([
-            'prefix'     => config('ward.uri', 'ward'),
-            'namespace'  => 'KABBOUCHI\Ward\Http\Controllers',
-            'middleware' => config('ward.middleware', 'web'),
-        ], function () {
-            $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
-        });
-    }
+	/**
+	 * Register the LogViewer routes.
+	 *
+	 * @return void
+	 */
+	protected function registerRoutes()
+	{
+		if (!config('ward.enable_routes')) return;
 
-    /**
-     * Register the LogViewer resources.
-     *
-     * @return void
-     */
-    protected function registerResources()
-    {
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'ward');
-    }
+		Route::group([
+			'prefix'     => config('ward.uri', 'ward'),
+			'namespace'  => 'KABBOUCHI\Ward\Http\Controllers',
+			'middleware' => config('ward.middleware', 'web'),
+		], function () {
+			$this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+		});
+	}
 
-    /**
-     * Define the asset publishing configuration.
-     *
-     * @return void
-     */
-    public function defineAssetPublishing()
-    {
-        $this->publishes([
-            WARD_PATH.'/public' => public_path('vendor/ward'),
-        ], 'ward-assets');
-    }
+	/**
+	 * Register the LogViewer resources.
+	 *
+	 * @return void
+	 */
+	protected function registerResources()
+	{
+		$this->loadViewsFrom(__DIR__ . '/../resources/views', 'ward');
+	}
 
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        if (! defined('WARD_PATH')) {
-            define('WARD_PATH', realpath(__DIR__.'/../'));
-        }
+	/**
+	 * Define the asset publishing configuration.
+	 *
+	 * @return void
+	 */
+	public function defineAssetPublishing()
+	{
+		$this->publishes([
+			WARD_PATH . '/public' => public_path('vendor/ward'),
+		], 'ward-assets');
+	}
 
-        $this->configure();
-        $this->offerPublishing();
-    }
+	/**
+	 * Register any application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		if (!defined('WARD_PATH')) {
+			define('WARD_PATH', realpath(__DIR__ . '/../'));
+		}
 
-    /**
-     * Setup the configuration for LogViewer.
-     *
-     * @return void
-     */
-    protected function configure()
-    {
-        $this->mergeConfigFrom(
-            __DIR__.'/../config/ward.php', 'ward'
-        );
-    }
+		$this->configure();
+		$this->offerPublishing();
+	}
 
-    /**
-     * Setup the resource publishing groups for LogViewer.
-     *
-     * @return void
-     */
-    protected function offerPublishing()
-    {
-        if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__.'/../config/ward.php' => config_path('ward.php'),
-            ], 'ward-config');
-        }
-    }
+	/**
+	 * Setup the configuration for LogViewer.
+	 *
+	 * @return void
+	 */
+	protected function configure()
+	{
+		$this->mergeConfigFrom(
+			__DIR__ . '/../config/ward.php', 'ward'
+		);
+	}
+
+	/**
+	 * Setup the resource publishing groups for LogViewer.
+	 *
+	 * @return void
+	 */
+	protected function offerPublishing()
+	{
+		if ($this->app->runningInConsole()) {
+			$this->publishes([
+				__DIR__ . '/../config/ward.php' => config_path('ward.php'),
+			], 'ward-config');
+		}
+	}
 }
